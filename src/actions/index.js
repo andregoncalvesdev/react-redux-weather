21b990c56b8fa10d75ccb55ca94a156508a86ce0
André Gonçalves <andre.goncalves@tekzenit.com>
2016-02-04 18:55:44 +0000

    wip

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8a9d87f18f11b2fa9e4c1cab7817cadb';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
